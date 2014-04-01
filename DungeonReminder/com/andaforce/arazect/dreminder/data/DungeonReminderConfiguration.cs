c52b99d121f6012e813198da61131410afd6c291
arazect <za3a24@yandex.ru>
2014-04-01 13:45:48 +0700

    Added full dungeon list

--- DungeonReminder/com/andaforce/arazect/dreminder/data/DungeonReminderConfiguration.cs
+++ DungeonReminder/com/andaforce/arazect/dreminder/data/DungeonReminderConfiguration.cs
@@ -14,9 +14,38 @@ namespace DungeonReminder.com.andaforce.arazect.dreminder.data
             LastUpdated = DateTime.Now;
             Dungeons = new List<Dungeon>()
             {
-                new Dungeon() {DungeonName = "Ascalonian Catacombs", PathName = "PathName 1", GoldReward = 1.5f},
-                new Dungeon() {DungeonName = "Ascalonian Catacombs", PathName = "PathName 2", GoldReward = 1.5f},
-                new Dungeon() {DungeonName = "Ascalonian Catacombs", PathName = "PathName 3", GoldReward = 1.5f}
+                new Dungeon() {DungeonName = "Ascalonian Catacombs", PathName = "Path 1 - Hodgins", GoldReward = 1.5f},
+                new Dungeon() {DungeonName = "Ascalonian Catacombs", PathName = "Path 2 - Detha", GoldReward = 1.5f},
+                new Dungeon() {DungeonName = "Ascalonian Catacombs", PathName = "Path 3 - Tzark", GoldReward = 1.5f},
+
+                new Dungeon() {DungeonName = "Caudecus's Manor", PathName = "Path 1 - Asura", GoldReward = 1.0f},
+                new Dungeon() {DungeonName = "Caudecus's Manor", PathName = "Path 2 - Seraph", GoldReward = 1.0f},
+                new Dungeon() {DungeonName = "Caudecus's Manor", PathName = "Path 3 - Butler", GoldReward = 1.0f},
+
+                new Dungeon() {DungeonName = "Twilight Arbor", PathName = "Path 1 - Leurent (Up)", GoldReward = 1.0f},
+                new Dungeon() {DungeonName = "Twilight Arbor", PathName = "Path 3 - Vevina (Forward)", GoldReward = 1.0f},
+                new Dungeon() {DungeonName = "Twilight Arbor", PathName = "Path 4 - Aetherpath", GoldReward = 2.0f},
+
+                new Dungeon() {DungeonName = "Sorrow's Embrace", PathName = "Path 1 - Fergg", GoldReward = 1.0f},
+                new Dungeon() {DungeonName = "Sorrow's Embrace", PathName = "Path 2 - Rasolov", GoldReward = 1.0f},
+                new Dungeon() {DungeonName = "Sorrow's Embrace", PathName = "Path 3 - Koptev", GoldReward = 1.0f},
+
+                new Dungeon() {DungeonName = "Citadel of Flame", PathName = "Path 1 - Ferrah", GoldReward = 1.0f},
+                new Dungeon() {DungeonName = "Citadel of Flame", PathName = "Path 2 - Magg", GoldReward = 1.0f},
+                new Dungeon() {DungeonName = "Citadel of Flame", PathName = "Path 3 - Rhiannon", GoldReward = 1.0f},
+
+                new Dungeon() {DungeonName = "Honor of the Waves", PathName = "Path 1 - Butcher", GoldReward = 1.0f},
+                new Dungeon() {DungeonName = "Honor of the Waves", PathName = "Path 2 - Plunderer", GoldReward = 1.0f},
+                new Dungeon() {DungeonName = "Honor of the Waves", PathName = "Path 3 - Zealot", GoldReward = 1.0f},
+
+                new Dungeon() {DungeonName = "Crucible of Eternity", PathName = "Path 1 - Submarine", GoldReward = 1.0f},
+                new Dungeon() {DungeonName = "Crucible of Eternity", PathName = "Path 2 - Teleporter", GoldReward = 1.0f},
+                new Dungeon() {DungeonName = "Crucible of Eternity", PathName = "Path 3 - Front d", GoldReward = 1.0f},
+
+                new Dungeon() {DungeonName = "The Ruined City of Arah", PathName = "Path 1 - Jotun", GoldReward = 3.0f},
+                new Dungeon() {DungeonName = "The Ruined City of Arah", PathName = "Path 2 - Mursaat", GoldReward = 3.0f},
+                new Dungeon() {DungeonName = "The Ruined City of Arah", PathName = "Path 3 - Forgotten", GoldReward = 1.5f},
+                new Dungeon() {DungeonName = "The Ruined City of Arah", PathName = "Path 4 - Seer", GoldReward = 3.0f}
             };
         }
     }
