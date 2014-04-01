26a4bbef0dcff0acbcd8be6ed160182cc2381235
arazect <za3a24@yandex.ru>
2014-04-01 14:04:41 +0700

    Changed appearance of form

--- DungeonReminder/com/andaforce/arazect/dreminder/forms/MainForm.Designer.cs
+++ DungeonReminder/com/andaforce/arazect/dreminder/forms/MainForm.Designer.cs
@@ -29,17 +29,24 @@
         private void InitializeComponent()
         {
             this.dgvDungeons = new System.Windows.Forms.DataGridView();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.tsslDate = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel2 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.tsslTotalGold = new System.Windows.Forms.ToolStripStatusLabel();
+            this.btnResetAll = new System.Windows.Forms.Button();
             this.Dungeon = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Path = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Reward = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Completed = new System.Windows.Forms.DataGridViewCheckBoxColumn();
             ((System.ComponentModel.ISupportInitialize)(this.dgvDungeons)).BeginInit();
+            this.statusStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // dgvDungeons
             // 
             this.dgvDungeons.AllowUserToAddRows = false;
             this.dgvDungeons.AllowUserToDeleteRows = false;
+            this.dgvDungeons.AllowUserToResizeRows = false;
             this.dgvDungeons.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dgvDungeons.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.Dungeon,
@@ -50,13 +57,54 @@
             this.dgvDungeons.Name = "dgvDungeons";
             this.dgvDungeons.RowHeadersVisible = false;
             this.dgvDungeons.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.dgvDungeons.Size = new System.Drawing.Size(620, 440);
+            this.dgvDungeons.Size = new System.Drawing.Size(620, 330);
             this.dgvDungeons.TabIndex = 1;
             this.dgvDungeons.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvDungeons_CellContentClick);
             this.dgvDungeons.CellValueChanged += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvDungeons_CellValueChanged);
             // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsslDate,
+            this.toolStripStatusLabel2,
+            this.tsslTotalGold});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 390);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(643, 22);
+            this.statusStrip1.TabIndex = 2;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // tsslDate
+            // 
+            this.tsslDate.Name = "tsslDate";
+            this.tsslDate.Size = new System.Drawing.Size(86, 17);
+            this.tsslDate.Text = "Last updated at ";
+            // 
+            // toolStripStatusLabel2
+            // 
+            this.toolStripStatusLabel2.Name = "toolStripStatusLabel2";
+            this.toolStripStatusLabel2.Size = new System.Drawing.Size(11, 17);
+            this.toolStripStatusLabel2.Text = "|";
+            // 
+            // tsslTotalGold
+            // 
+            this.tsslTotalGold.Name = "tsslTotalGold";
+            this.tsslTotalGold.Size = new System.Drawing.Size(74, 17);
+            this.tsslTotalGold.Text = "Total gold = 0";
+            // 
+            // btnResetAll
+            // 
+            this.btnResetAll.Location = new System.Drawing.Point(557, 348);
+            this.btnResetAll.Name = "btnResetAll";
+            this.btnResetAll.Size = new System.Drawing.Size(75, 23);
+            this.btnResetAll.TabIndex = 3;
+            this.btnResetAll.Text = "Reset All";
+            this.btnResetAll.UseVisualStyleBackColor = true;
+            this.btnResetAll.Click += new System.EventHandler(this.btnResetAll_Click);
+            // 
             // Dungeon
             // 
+            this.Dungeon.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.None;
             this.Dungeon.DataPropertyName = "DungeonName";
             this.Dungeon.HeaderText = "Dungeon";
             this.Dungeon.Name = "Dungeon";
@@ -65,6 +113,7 @@
             // 
             // Path
             // 
+            this.Path.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.None;
             this.Path.DataPropertyName = "PathName";
             this.Path.HeaderText = "Path Name";
             this.Path.Name = "Path";
@@ -73,6 +122,7 @@
             // 
             // Reward
             // 
+            this.Reward.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.None;
             this.Reward.DataPropertyName = "GoldReward";
             this.Reward.HeaderText = "Gold Reward";
             this.Reward.Name = "Reward";
@@ -80,6 +130,7 @@
             // 
             // Completed
             // 
+            this.Completed.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.None;
             this.Completed.DataPropertyName = "IsCompleted";
             this.Completed.HeaderText = "Completed";
             this.Completed.Name = "Completed";
@@ -90,20 +141,32 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(648, 480);
+            this.ClientSize = new System.Drawing.Size(643, 412);
+            this.Controls.Add(this.btnResetAll);
+            this.Controls.Add(this.statusStrip1);
             this.Controls.Add(this.dgvDungeons);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
             this.Name = "MainForm";
             this.Text = "MainForm";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing);
             this.Load += new System.EventHandler(this.MainForm_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dgvDungeons)).EndInit();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.DataGridView dgvDungeons;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel tsslDate;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel2;
+        private System.Windows.Forms.ToolStripStatusLabel tsslTotalGold;
+        private System.Windows.Forms.Button btnResetAll;
         private System.Windows.Forms.DataGridViewTextBoxColumn Dungeon;
         private System.Windows.Forms.DataGridViewTextBoxColumn Path;
         private System.Windows.Forms.DataGridViewTextBoxColumn Reward;
